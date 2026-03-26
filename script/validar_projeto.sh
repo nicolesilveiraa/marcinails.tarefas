9116fdb7d1b3e826c8d586554ee23406b766c80d
Nicole Silveira <nicolesilveira415@gmail.com>
2026-03-26 10:39:58 -0300

    chore: adicionando script de validacao de ambiente

---
+++ script/validar_projeto.sh
@@ -0,0 +1,4 @@
+echo "Iniciando validacao de itens de configuracao..."
+echo "Verificando arquivos em src/..."
+[ -f "src/index.html" ] && echo "index.html encontrado!"
+echo "Sistema pronto para CI."
